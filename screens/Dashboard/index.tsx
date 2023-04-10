09c2b8e63e3d6c85401d0b2ec23001b50850c49b
Joseph Harris <joeharris461767@gmail.com>
2023-04-10 22:53:25 +0100

    Add iso function with tz Implement new iso function

--- screens/Dashboard/index.tsx
+++ screens/Dashboard/index.tsx
@@ -44,11 +44,29 @@ const Dashboard = () => {
     return date.split("T")[0];
   };
 
+  // This has been taken from https://stackoverflow.com/a/17415677
+  const toIsoString = (date: Date) => {
+    var tzo = -date.getTimezoneOffset(),
+        dif = tzo >= 0 ? '+' : '-',
+        pad = function(num) {
+            return (num < 10 ? '0' : '') + num;
+        };
+  
+    return date.getFullYear() +
+        '-' + pad(date.getMonth() + 1) +
+        '-' + pad(date.getDate()) +
+        'T' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes()) +
+        ':' + pad(date.getSeconds()) +
+        dif + pad(Math.floor(Math.abs(tzo) / 60)) +
+        ':' + pad(Math.abs(tzo) % 60);
+  }
+
   const formatCallsAsItems = () => {
     const events: AgendaSchedule = {};
 
     calls.forEach((call) => {
-      const formattedDate = getDateString(new Date(call.date).toISOString());
+      const formattedDate = getDateString(toIsoString(new Date(call.date)));
 
       const day = formattedDate.split("-")[2];
 
